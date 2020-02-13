cae170890e3981be79bbb2b3debcccd859e57534
tailordhruv <dhruv.tailor@coviam.com>
2020-02-13 15:28:59 +0530

    dhruv1 commit

--- src/java8/LambdaExpressionExample1.java
+++ src/java8/LambdaExpressionExample1.java
@@ -5,8 +5,8 @@ interface Drawable{
 }
 public class LambdaExpressionExample1 {
     public static void main(String[] args) {
-        Drawable d= w -> System.out.println("Drawing "+w);
-        d.draw(5);
+        Drawable drawable= w -> System.out.println("Drawing "+w);
+        drawable.draw(5);
 
     }
 }
